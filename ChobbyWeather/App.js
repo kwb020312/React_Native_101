9d632eb65e3c5199d9a8c5580943543b00c780f7
kwb020312 <kwb020312@naver.com>
2021-10-08 22:06:47 +0900

    #2.6 Styles part Two

--- ChobbyWeather/App.js
+++ ChobbyWeather/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
 	return (
@@ -7,12 +9,29 @@ export default function App() {
 			<View style={styles.city}>
 				<Text style={styles.cityName}>Seoul</Text>
 			</View>
-			<View style={styles.weather}>
+			<ScrollView
+				showsHorizontalScrollIndicator={false}
+				pagingEnabled
+				horizontal
+				contentContainerStyle={styles.weather}
+			>
 				<View style={styles.day}>
 					<Text style={styles.temp}>27</Text>
 					<Text style={styles.description}>Sunny</Text>
 				</View>
-			</View>
+				<View style={styles.day}>
+					<Text style={styles.temp}>27</Text>
+					<Text style={styles.description}>Sunny</Text>
+				</View>
+				<View style={styles.day}>
+					<Text style={styles.temp}>27</Text>
+					<Text style={styles.description}>Sunny</Text>
+				</View>
+				<View style={styles.day}>
+					<Text style={styles.temp}>27</Text>
+					<Text style={styles.description}>Sunny</Text>
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
@@ -24,26 +43,22 @@ const styles = StyleSheet.create({
 	},
 	city: {
 		flex: 1.2,
-		backgroundColor: 'blue',
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
 	cityName: {
-		fontSize: 68,
+		fontSize: 58,
 		fontWeight: '500'
 	},
-	weather: {
-		flex: 3,
-		backgroundColor: 'teal'
-	},
+	weather: {},
 	day: {
-		backgroundColor: 'teal',
-		flex: 1,
-		marginTop: -30,
+		width: SCREEN_WIDTH,
 		alignItems: 'center'
 	},
 	temp: {
-		fontSize: 178
+		fontSize: 178,
+		marginTop: 50,
+		fontWeight: '600'
 	},
 	description: {
 		fontSize: 60
